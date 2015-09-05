cc0edfb057e39b6080ec3b5ada5739f455ecfe4a
kevinezequiel <ezequielborgobello@gmail.com>
2015-09-05 10:20:56 -0300

    Created by Coding Ground Client

---
+++ New Project/Animal.java
@@ -0,0 +1,39 @@
+public class Animal
+
+    //--propiedades
+    public String nombre;
+    public Integer cantidad de patas;
+    public String tipo;
+    public String sonido;
+    
+
+    //------------------CONSTRUCTOR
+    public Animal() {
+        
+        
+        
+        //-------------------metodos
+        public void caminar(){
+            System.out.println("Metodo Caminar" + distancia);
+        }
+        
+        public Integer correr(){
+            return true;
+        }
+        
+        reproducirsonido
+        banarse
+        
+        
+        
+        
+        
+        
+    }
+
+
+
+
+
+
+
